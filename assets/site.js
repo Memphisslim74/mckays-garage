7e9bec1c28a7ac43bc121771b0a33cfca47e8581
Steve Smith <150203199+Memphisslim74@users.noreply.github.com>
2026-09-10 09:45:06 -0500

    Version gallery CSS to bust stale cache

--- assets/site.js
+++ assets/site.js
@@ -77,7 +77,7 @@ function addShopGallery() {
 
   const css = document.createElement('link');
   css.rel = 'stylesheet';
-  css.href = '/assets/gallery.css';
+  css.href = '/assets/gallery.css?v=20260910-4';
   document.head.appendChild(css);
 
   const isHome = location.pathname === '/' || location.pathname === '/index.html';
